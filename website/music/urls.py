13cc56a5c9f8e34377343d41e26640a52deb2eea
jens5267 <jensreichwein@web.de>
2020-01-08 22:35:39 +0100

    finished video 12

--- website/music/urls.py
+++ website/music/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
+    # /music/
     url(r'^$', views.index, name='index'),
+    # /music/<id>
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='index-detail'),
 ]
 
--- website/music/views.py
+++ website/music/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('test')
+
+def detail(request, album_id):
+    return HttpResponse('details for album id: ' + str(album_id))

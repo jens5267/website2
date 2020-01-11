00fe61188ec1fe0b6e181e4e67be645a15534d1b
jens5267 <jensreichwein@web.de>
2020-01-11 20:38:51 +0100

    finished video 24

--- website/music/urls.py
+++ website/music/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url
 from . import views
 
+app_name = 'music'
 urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),
     # /music/<id>
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='index-detail'),
+    # /music/<album_id>/favorite
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='index-favorite'),
 ]
 
